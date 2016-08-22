13d860ce05300fb32e4c104a8c4d264b0c5705ef
Alex DeMars <alexdemars@gmail.com>
2016-08-22 15:46:56 -0700

    initial commit

---
+++ WatchDollar/CustomTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  CustomTabBarController.swift
+//  WatchDollar
+//
+//  Created by Alex DeMars on 8/22/16.
+//  Copyright © 2016 Alex DeMars. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = UIColor(red: 0, green: 0.57, blue: 0.63, alpha: 1)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WatchDollar/ExpenseViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ExpenseViewController.swift
+//  WatchDollar
+//
+//  Created by Alex DeMars on 8/22/16.
+//  Copyright © 2016 Alex DeMars. All rights reserved.
+//
+
+import UIKit
+
+class ExpenseViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        return cell
+    }
+
+}
+
